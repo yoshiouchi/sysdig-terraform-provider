eea8e831d1082ee43dde53dde43c9620bf6fb4c1
Yoshi Ouchi <yoshi.ouchi@sysdig.com>
2025-02-26 17:11:50 +1100

    Fixed the provider details

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     sysdig = {
-      source  = "sysdiglabs/sysdig"
-      version = "~> 1.0"
+      source = "sysdiglabs/sysdig"
+      version = ">=0.5"
     }
   }
 }
 
 provider "sysdig" {
   sysdig_secure_api_token = var.SYSDIG_SECURE_API_TOKEN
-  sysdig_secure_endpoint_url = var.SYSDIG_SECURE_ENDPOINT_URL
+  sysdig_secure_url = var.SYSDIG_SECURE_ENDPOINT_URL
 }
 
 module "sysdig_secure_posture_accept_risk" {
