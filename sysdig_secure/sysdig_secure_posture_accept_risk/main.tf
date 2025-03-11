3c17a5a6aa97c9b809873844f0ca2d3ae57d44ae
Yoshi Ouchi <yoshi.ouchi@sysdig.com>
2025-03-11 22:11:53 +1100

    Corrected the order

--- sysdig_secure/sysdig_secure_posture_accept_risk/main.tf
+++ sysdig_secure/sysdig_secure_posture_accept_risk/main.tf
@@ -1,15 +1,16 @@
 resource "sysdig_secure_posture_accept_risk" "entire_infrastructure_sysdig_kubernetes" {
-    zone_name = "Entire Infrastructure"
-    control_name = "ServiceAccounts with cluster access"
     description  = "Accept risk for Entire Infrastructure/Sysdig Kubernetes/3.15 Minimize out of scope access"
+    control_name = "ServiceAccounts with cluster access"
     reason       = "Risk Not Relevant"
     expires_in   = "30 Days"
     filter = "namespace in (\"argocd\")"
-}
-resource "sysdig_secure_posture_accept_risk" "all_posture_findings_linux_controls" {
     zone_name    = "Entire Infrastructure"
-    control_name = "/etc/sysctl.conf or /etc/sysctl.d/* contains appropriate `net.ipv4.icmp_ignore_bogus_error_responses` setting"
+}
+
+resource "sysdig_secure_posture_accept_risk" "all_posture_findings_linux_controls" {
     description  = "Accept risk for RFC-1122"
+    control_name = "/etc/sysctl.conf or /etc/sysctl.d/* contains appropriate `net.ipv4.icmp_ignore_bogus_error_responses` setting"
     reason       = "Risk Owned"
     expires_in   = "Never"
+    zone_name    = "Entire Infrastructure"
 }
