3da02673e69ef8ca1ed989662b2bc7c92aaebe83
Yoshi Ouchi <yoshi.ouchi@sysdig.com>
2025-03-11 22:00:45 +1100

    Added a new risk acceptance

--- sysdig_secure/sysdig_secure_posture_accept_risk/main.tf
+++ sysdig_secure/sysdig_secure_posture_accept_risk/main.tf
@@ -1,8 +1,17 @@
 resource "sysdig_secure_posture_accept_risk" "entire_infrastructure_sysdig_kubernetes" {
-    description  = "Accept risk for Entire Infrastructure/Sysdig Kubernetes/3.15 Minimize out of scope access"
+    id = "100"
+    zone_name = "Entire Infrastructure"
     control_name = "ServiceAccounts with cluster access"
+    description  = "Accept risk for Entire Infrastructure/Sysdig Kubernetes/3.15 Minimize out of scope access"
     reason       = "Risk Not Relevant"
     expires_in   = "30 Days"
     filter = "namespace in (\"argocd\")"
-    zone_name = "Entire Infrastructure"
+}
+resource "sysdig_secure_posture_accept_risk" "all_posture_findings_linux_controls" {
+    id = "101"
+    zone_name    = "Entire Infrastructure"
+    control_name = "/etc/sysctl.conf or /etc/sysctl.d/* contains appropriate `net.ipv4.icmp_ignore_bogus_error_responses` setting"
+    description  = "Accept risk for RFC-1122"
+    reason       = "Risk Owned"
+    expires_in   = "Never"
 }
