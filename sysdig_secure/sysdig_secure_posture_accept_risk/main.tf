c26345652f414720b17d52d71a2d15166c2f1282
Yoshi Ouchi <yoshi.ouchi@sysdig.com>
2025-03-11 22:19:19 +1100

    Removed the 2nd resource

--- sysdig_secure/sysdig_secure_posture_accept_risk/main.tf
+++ sysdig_secure/sysdig_secure_posture_accept_risk/main.tf
@@ -6,11 +6,3 @@ resource "sysdig_secure_posture_accept_risk" "entire_infrastructure_sysdig_kuber
     filter = "namespace in (\"argocd\")"
     zone_name    = "Entire Infrastructure"
 }
-
-resource "sysdig_secure_posture_accept_risk" "all_posture_findings_linux_controls" {
-    description  = "Accept risk for RFC-1122"
-    control_name = "/etc/sysctl.conf or /etc/sysctl.d/* contains appropriate `net.ipv4.icmp_ignore_bogus_error_responses` setting"
-    reason       = "Risk Owned"
-    expires_in   = "Never"
-    zone_name    = "Entire Infrastructure"
-}
