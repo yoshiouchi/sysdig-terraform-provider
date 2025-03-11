e8ee3031c3b01de948e250ae72fbdb683ac6ef20
Yoshi Ouchi <yoshi.ouchi@sysdig.com>
2025-03-11 22:02:10 +1100

    Corrected the id values

--- sysdig_secure/sysdig_secure_posture_accept_risk/main.tf
+++ sysdig_secure/sysdig_secure_posture_accept_risk/main.tf
@@ -1,5 +1,5 @@
 resource "sysdig_secure_posture_accept_risk" "entire_infrastructure_sysdig_kubernetes" {
-    id = "100"
+    id = 100
     zone_name = "Entire Infrastructure"
     control_name = "ServiceAccounts with cluster access"
     description  = "Accept risk for Entire Infrastructure/Sysdig Kubernetes/3.15 Minimize out of scope access"
@@ -8,7 +8,7 @@ resource "sysdig_secure_posture_accept_risk" "entire_infrastructure_sysdig_kuber
     filter = "namespace in (\"argocd\")"
 }
 resource "sysdig_secure_posture_accept_risk" "all_posture_findings_linux_controls" {
-    id = "101"
+    id = 101
     zone_name    = "Entire Infrastructure"
     control_name = "/etc/sysctl.conf or /etc/sysctl.d/* contains appropriate `net.ipv4.icmp_ignore_bogus_error_responses` setting"
     description  = "Accept risk for RFC-1122"
