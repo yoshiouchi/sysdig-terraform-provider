d071873fa26bb29246465d5e7891c03226acdb83
Yoshi Ouchi <yoshi.ouchi@sysdig.com>
2025-03-12 16:51:31 +1100

    Changed the expiry date

--- sysdig_secure/sysdig_secure_posture_accept_risk/main.tf
+++ sysdig_secure/sysdig_secure_posture_accept_risk/main.tf
@@ -2,7 +2,7 @@ resource "sysdig_secure_posture_accept_risk" "entire_infrastructure_sysdig_kuber
     description  = "Accept risk for Entire Infrastructure/Sysdig Kubernetes/3.15 Minimize out of scope access"
     control_name = "ServiceAccounts with cluster access"
     reason       = "Risk Not Relevant"
-    expires_in   = "30 Days"
+    expires_in   = "7 Days"
     filter = "namespace in (\"argocd\")"
     zone_name    = "Entire Infrastructure"
 }
